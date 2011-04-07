8e63da98e15c51a69aba8f5092f81b4924a8398d
Matt Goodall <matt.goodall@gmail.com>
2011-04-07 11:23:46 +0100

    Make log level configurable.

--- forkd.py
+++ forkd.py
@@ -263,6 +263,9 @@ def main():
 
     # Parse forkd args.
     parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--log-level', metavar='log_level',
+                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                        default='WARNING', help='log level')
     parser.add_argument('-n', '--num-workers', metavar='num_workers', type=int,
                         default=1, help='initial number of worker processes')
     parser.add_argument('worker',
@@ -271,11 +274,12 @@ def main():
                         help='args passed to the worker function via sys.argv')
     args = parser.parse_args()
 
+    logging.basicConfig(level=getattr(logging, args.log_level))
+
     manager = Forkd(args.worker, num_workers=args.num_workers)
     sys.argv[:] = [args.worker] + args.worker_args
     manager.run()
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
     main()
